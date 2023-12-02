610250c0ca7a04db093ebbaba476e78b360257d6
Dennis Riis <dr@driis.dk>
2023-12-02 13:17:01 +0100

    Debugged and cleaned up day1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,31 +1,28 @@
-﻿var digitStrings = new[]
+﻿var tokens = new (string,int)?[]
 {
     ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9),
     ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)
 };
-int[] SelectDigitsFromLine(string line)
-{
-    List<Digit> digits = new List<Digit>();
-    foreach (var t in digitStrings)
-    {
-        var offset = line.IndexOf(t.Item1);
-        if (offset < 0)
-            continue;
-        digits.Add(new(offset, t));
-    }
 
-    return digits.OrderBy(d => d.Offset).Select(d => d.Tuple.Item2).ToArray();
+(string,int)? PickTokenFromStart(string input) => tokens.SingleOrDefault(t => input.StartsWith(t!.Value.Item1));
+
+int DigitsFromLine(string line)
+{
+    var digits = new List<(string,int)>();
+    int offset = 0;
+    while(line.Length > 0)
+    {
+        var token = PickTokenFromStart(line);
+        if (token != null)
+            digits.Add(token.Value);
+        line = line[1..];
+        offset++;
+    }
+    
+    return Convert.ToInt32($"{digits.First().Item2}{digits.Last().Item2}");
+
 }
 var input = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt");
-var coords = input.Select(line =>
-{
-    var digits = SelectDigitsFromLine(line);
-    string it = $"{digits.First()}{digits.Last()}";
-    Console.WriteLine($"{line}\t{it}");
-    return Convert.ToInt32(it);
-}).ToArray();
-
+var coords = input.Select(DigitsFromLine);
 var answer = coords.Sum();
 Console.WriteLine(answer);
-
-record Digit(int Offset,(string,int) Tuple);
