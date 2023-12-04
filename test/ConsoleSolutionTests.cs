70c80c1c4bd0acb37d9be47e0648c390abc5a504
Dennis Riis <dr@driis.dk>
2023-12-04 21:14:22 +0100

    Add test, (simply running the stand-alone daily solutions)

---
+++ test/ConsoleSolutionTests.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+
+namespace test;
+
+public class Tests
+{
+    [TestCaseSource(nameof(EnumerateDays))]
+    public async Task RunSolutions(string path)
+    {
+        Console.WriteLine($"Running solution in {path}");
+        
+        var p = Process.Start(new ProcessStartInfo("dotnet", "run --no-build")
+        {
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            WorkingDirectory = path
+        });
+        await p.WaitForExitAsync();
+        Console.WriteLine(await p.StandardOutput.ReadToEndAsync());
+        var errorsOut = await p.StandardError.ReadToEndAsync();
+        if (!String.IsNullOrWhiteSpace(errorsOut))
+        {
+            Console.WriteLine("StdErr out: was:\n----------");
+            Console.WriteLine(errorsOut);
+        }
+        Assert.That(p.ExitCode, Is.EqualTo(0), $"Process did not exit with code 0, but {p.ExitCode}");
+    }
+
+    public static IEnumerable<string> EnumerateDays()
+    {
+        int maxDepth = 10;
+        string dir = Environment.CurrentDirectory;
+        bool found = false;
+        while (!found && maxDepth > 0)
+        {
+            dir = Directory.GetParent(dir)?.FullName ?? throw new ApplicationException($"No parent for {dir}");
+            var slnFiles = Directory.EnumerateFiles(dir, "*.sln");
+            found = slnFiles.Any();
+            maxDepth--;
+        }
+
+        if (!found)
+            throw new ApplicationException("Unable to find sln dir");
+
+        var directories = Directory.EnumerateDirectories(dir, "day*");
+        return directories;
+    }
+}
