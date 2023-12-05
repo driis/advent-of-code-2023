d32303ea9508c67cd0a1e49376edf8e71caed9f1
Dennis Riis <dr@driis.dk>
2023-12-05 08:44:30 +0100

    prep day5

---
+++ day5/Usings.cs
@@ -0,0 +1,28 @@
+global using static System.Console;
+global using System.Text.RegularExpressions;
+global using System.IO;
+
+public static class Extensions
+{
+    public static int ToInt(this string value)
+    {
+        return int.TryParse(value, out int n) ? 
+            n : 
+            throw new ApplicationException($"{value} could not be parsed as an integer");
+    }
+
+        public static int? ToIntNullable(this string value)
+    {
+        return int.TryParse(value, out int n) ? 
+            n : 
+            null;
+    }
+
+    public static void DumpConsole<T>(IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            WriteLine(item);
+        }
+    }
+}
