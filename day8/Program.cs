ff13ece5497957349d4d5876709b4a2590897821
Dennis Riis <dr@driis.dk>
2023-12-12 13:57:47 +0100

    day8

--- day8/Program.cs
+++ day8/Program.cs
@@ -1,30 +1,54 @@
 ﻿// Normally we will start by reading lines from an input file
+
+
+using System.Diagnostics;
+
 var input = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt");
 var instructions = input[0];
 var locations = input[2..].Select(Node.Parse).ToArray();
 var lookup = locations.ToDictionary(x => x.Code);
-bool doPartOne = lookup.TryGetValue("AAA", out var pos);
-int count = 0;
 
-while (doPartOne && pos.Code != "ZZZ")
+long Solve(Node pos, Func<Node, bool> goal)
 {
-    var instr = instructions[count++ % instructions.Length];
-    var next = instr == 'R' ? pos.Right : pos.Left;
-    pos = lookup[next];
+    int count = 0;
+    while (!goal(pos))
+    {
+        var instr = instructions[count++ % instructions.Length];
+        var next = instr == 'R' ? pos.Right : pos.Left;
+        pos = lookup[next];
+    }
+
+    return count;
 }
-WriteLine($"Part 1: Steps used {count}");
+bool doPartOne = lookup.TryGetValue("AAA", out var node);
+if (doPartOne)
+{ 
+    var answer = Solve(node!, n => n.Code == "ZZZ");
+    WriteLine($"Part 1: Steps used {answer}");
+}
+
+long gcf(long a, long b)
+{
+    while (b != 0)
+    {
+        var temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+long lcm(long a, long b) => (a / gcf(a, b)) * b;
+
 // Part 2
-count = 0;
 var nodes = locations.Where(x => x.IsStartingNode).ToArray();
 WriteLine($"Part 2 starting with {nodes.Length} nodes: {String.Join(", ", nodes.Select(x=>x.Code))}.");
-while(!nodes.All(x => x.IsEndNode))
-{
-    var instr = instructions[count++ % instructions.Length];
-    // Move all the nodes
-    var nextCodes = nodes.Select(node => instr == 'R' ? node.Right : node.Left).ToArray();
-    nodes = nextCodes.Select(n => lookup[n]).ToArray();
-}
-WriteLine($"Part 2: Steps used {count}");
+var answers = nodes.Select(n => Solve(n, x => x.IsEndNode)).ToArray();
+WriteLine("Individual counts:");
+answers.DumpConsole();
+WriteLine("Finding least common multiple");
+var answerPartTwo = answers.Aggregate(1L, (a, b) => lcm(a, b));
+WriteLine($"Part 2 Answer: {answerPartTwo}");
 public record Node(string Code, string Left, string Right)
 {
     private static readonly Regex regParse = new Regex(@"(\w+) = \((\w+), (\w+)\)");
